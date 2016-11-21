6c72dcbd3fd79f6be491dbf150a46608aca6a9de
bbrodsky <bbrodsky@gmail.com>
2016-11-21 11:17:30 -0500

    login/logout button changes

--- browser/react/components/Sidebar.js
+++ browser/react/components/Sidebar.js
@@ -11,16 +11,27 @@ class Sidebar extends React.Component {
         return (
             <div>
                 <Link to='/video'>Video</Link>
+                {this.props.user ?
                 <Button onClick={this.props.logoutClick}>Log out</Button>
+                :
+                <div>
+                  <Link to="/login"><Button>Log in</Button></Link>
+                  <Link to="/signup"><Button>Sign Up</Button></Link>
+                </div>
+                }
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => ({
+  user: state.auth
+})
 
 const mapDispatch = dispatch => ({
   logoutClick: () => {
     dispatch(logout());
   }
 })
-const SideBarContainer = connect(null, mapDispatch)(Sidebar);
+const SideBarContainer = connect(mapStateToProps, mapDispatch)(Sidebar);
 export default SideBarContainer;
