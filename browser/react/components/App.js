f2e429dae04f0dc3bc5b084fba46421dac6989f2
Colten Jackson <jazzyjackson@gmail.com>
2016-11-21 14:41:04 -0500

    unContainerized Navbar

--- browser/react/components/App.js
+++ browser/react/components/App.js
@@ -8,15 +8,17 @@ export default class App extends Component {
 
   render () {
     return (
-      <div id="main" className="container-fluid">
-        <div className='row'> <Navbar/> </div>
-        <div className='row'>
-          <div className="col-xs-2">
-          </div>
-          <div className="col-xs-8">
-            { this.props.children }
-          </div>
-          <div className="col-xs-2">
+      <div>
+        <Navbar/> 
+        <div id="main" className="container">
+          <div className='row'>
+            <div className="col-xs-2">
+            </div>
+            <div className="col-xs-8">
+              { this.props.children }
+            </div>
+            <div className="col-xs-2">
+            </div>
           </div>
         </div>
       </div>
