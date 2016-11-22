25b9da5437fb918b0714e6743238b149ce6313a0
Colten Jackson <jazzyjackson@gmail.com>
2016-11-22 16:15:15 -0500

    theres a ads reducer now

---
+++ browser/react/components/Ads/Ads.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+
+export default class Ads extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    console.log(this.props)
+  }
+
+  componentDidMount(){
+    this.props.findAds();
+  } 
+
+  render() {
+    
+    let {user,ads} = this.props;
+    
+    let choiceA = ads[0],
+        choiceB = ads[1];
+    return (
+      <div id="ads">
+        <ul id="slide-out" className="side-nav fixed">
+          <li>
+            <div className="userView">
+              <div className="background">
+              </div>
+              <a href="#!user"><img className="circle" src="img/smile/1.jpg"/></a>
+              <a href="#!name"><span className="black-text name">John Doe</span></a>
+              <a href="#!email"><span className="black-text email">{user ? user.email : "Props didnt happen"}</span></a>
+              <a href="#!email"><span className="black-text">Watched # ads</span></a>
+              <a href="#!email"><span className="black-text">Earned # smilyPoints</span></a>
+            </div>
+          </li>
+          <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
+          <li><a href="#!">Second Link</a></li>
+          <li><div className="divider"></div></li>
+          <li><a className="subheader">Subheader</a></li>
+          <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+        </ul>
+        <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
+        <div className="container videocards">
+          <div className="row">
+            <div className="col s6">
+              <div className="card">
+                <div className="card-image">
+                  <img src={`http://img.youtube.com/vi/${urlA}/maxresdefault.jpg`}/>
+                  <span className="card-title">Ad 1</span>
+                </div>
+                <div className="card-content">
+                  <p>Ad on cool thing #1</p>
+                </div>
+                <div className="card-action">
+                  <a href={`/video?${urlA}`}>Watch Option A</a>
+                </div>
+              </div>
+            </div>
+            <div className="col s6">
+              <div className="card">
+                <div className="card-image">
+                  <img src={`http://img.youtube.com/vi/${urlB}/maxresdefault.jpg`}/>
+                  <span className="card-title">Ad 2</span>
+                </div>
+                <div className="card-content">
+                  <p>Ad on cool thing #2</p>
+                </div>
+                <div className="card-action">
+                  <a href={`/video?${urlB}`}>Watch Option B</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
