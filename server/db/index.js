2d2f0d995e5c997fc3f9b9b59bc2509c14d30947
Sara Sunshine <sarasunsh@gmail.com>
2016-11-23 11:25:22 -0500

    Sequelize error squashed

--- server/db/index.js
+++ server/db/index.js
@@ -6,16 +6,11 @@ const chalk = require('chalk');
 // so any other part of the application can simply call sequelize.model('User')
 // to get access to the User model.
 var Models = require('./models');
-const adSeed = () =>Models.Ad.create({
-    title: "Rogue 1 Trailer",
-    url: "WcwsDJhrREU"
-});
+
 // Syncing all the models at once. This promise is used by main.js.
 var syncedDbPromise = db.sync(
   // {force:true}
 )
-.then(adSeed);
-
 
 syncedDbPromise.then(function () {
   console.log(chalk.green('Sequelize models synced to PostgreSQL'));
