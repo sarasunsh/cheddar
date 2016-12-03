b5c52aeb106458b0221b11e7ac0bf905c0a1ed5d
Sara Sunshine <sarasunsh@gmail.com>
2016-12-03 12:28:01 -0500

    Enzyme tests

--- browser/react/reducers/adFromClick.js
+++ browser/react/reducers/adFromClick.js
@@ -20,17 +20,3 @@ export default function reducer (adChoice = {url: "", title: "", id: 0}, action)
     }
 }
 
-/* ------------       DISPATCHERS     ------------------ */
-
-export const findAds = () => dispatch => {
-    //respond to get api ads next should be an array of ad urls
-    axios.get('/api/videos')
-        .then(res => { 
-            //the server will return an array of 2 objects representing ads. url, title, id.
-            dispatch(setAds(res.data))
-        })
-        .catch(err => {
-            console.error(err);
-          //  dispatch(setAds(['WcwsDJhrREU','MaYv3Y8tyoQ']))
-        })
-}
