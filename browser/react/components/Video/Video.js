816cbedd1aa48f9708c3937aad87e49453ede3fe
Colten Jackson <jazzyjackson@gmail.com>
2016-12-06 12:43:45 -0500

    Hide the youtube controls

--- browser/react/components/Video/Video.js
+++ browser/react/components/Video/Video.js
@@ -19,6 +19,7 @@ export default class Video extends React.Component {
         this.height = 480;
         this.url = `https://www.youtube.com/embed/${this.props.currentAd.url}?`;
         this.url += 'enablejsapi=1&' // must be appended to embed url so we can control play/pause
+        this.url =+ 'controls=0&'
         this.url += 'showinfo=0&' // and controls=0 will hide the youtube player controls
         this.url += 'disablekb=1&' // will prevent you from skipping ahead with arrow keys
         this.url += 'iv_load_policy=3&' // hides video annotations / popups / subscribe now stuff
